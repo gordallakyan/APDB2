5ebd5871b2183a8f85ed287eef591c212848b076
gordallakyan <109466778+gordallakyan@users.noreply.github.com>
2024-03-13 20:06:21 +0100

    Srednia

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("World!");
+
+
+double average(double[] arr)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
+
+Console.WriteLine(average([1,2,3,4,5]));
